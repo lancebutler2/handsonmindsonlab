1c07c7abb40332022c1028f86a081fa132c2c08f
Lance Butler II <lancebutler2@gmail.com>
2016-07-11 22:32:11 -0400

    link the home hero button to the blog

--- template-parts/components/home-hero.php
+++ template-parts/components/home-hero.php
@@ -5,6 +5,6 @@
         <h1 class="Logo">
             <span class="Logo__word">Hands</span> <span class="Logo__word">on</span> <span class="Logo__word">Minds</span> <span class="Logo__word">on</span> <span class="Logo__word">Lab</span>
         </h1>
-        <span><a class="HomeHero__button"><i class="HomeHero__button-bulb fa fa-lightbulb-o fa-fw"></i>Imagineers</a></span>
+        <span><a class="HomeHero__button" href="<?php echo home_url('/blog'); ?>" title="View the blog"><i class="HomeHero__button-bulb fa fa-lightbulb-o fa-fw"></i>Imagineers</a></span>
     </div>
 </div>
